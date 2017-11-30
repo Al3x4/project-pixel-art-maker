6c3323eeec4fad9bb75e7412a2377704702eebed
btkalex <alexandrapreda89@gmail.com>
2017-11-30 20:33:25 +0000

    Create makeGrid function that works on click

--- designs.js
+++ designs.js
@@ -2,9 +2,24 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
+$(":submit").click(makeGrid);
 function makeGrid() {
-
-// Your code goes here!
+  //clears existing table
+  $("#pixel_canvas").empty();
+  //prevent page from refreshing
+  event.preventDefault();
+  //get values for table width and height
+  const tableWidth = $("#input_width").val();
+  const tableHeight = $("#input_height").val();
+  //create tableHeight tr's
+  for (let y=0; y<tableHeight; y++){
+    $("#pixel_canvas").append("<tr></tr>");
+    console.log("have row");
+    //create tableWidth td's
+    for (let x=0; x<tableWidth; x++){
+      //insert cell into html
+      $("#pixel_canvas > tr:last").append("<td></td>");
+    }
+  }
 
 }
